3439ac8f6957b5e1fe94539ce9f5335e32e0905e
Corey Riviello <riviellocorey@gmail.com>
2022-11-06 02:33:52 -0500

    Initialized redux store, set up provider. Created post reducer with initial state and instantiated actions.

---
+++ frontend/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postSlice from '../features/posts/postSlice';
+
+export const store = configureStore({
+    reducer: {
+        posts: postSlice
+    }
+});
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,5 +7,9 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
 
