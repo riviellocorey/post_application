e210cdadc9f986e2c1467b2d7efac5e83caac87d
Corey Riviello <riviellocorey@gmail.com>
2022-11-06 02:25:31 -0500

    Initial frontend with dependencies added for Redux.
    Create basic hello world React application with Redux packages installed. Sorted out basic file structure for future development. Changed public details such as favicon. Removed unnecessary files.

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import { Provider } from 'react-redux';
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
