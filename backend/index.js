8555d51501501c9ecd831ab8e2fefb1e828208aa
Corey Riviello <riviellocorey@gmail.com>
2022-11-06 03:01:55 -0500

    Added Jest and Supertest testing
    Included two tests that check if the get request to /api/v1/posts returns 200 and JSON.

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,4 @@
-const express = require("express");
-const { PORT, NODE_ENV } = require("./config/Environments");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-
-// @ Posts Route
-app.use("/api/v1/posts", require("./routes/posts"));
-
-app.use((err, req, res, next) => {
-    if (NODE_ENV === "development") {
-        res.status(err.status).json({ status: "failed", data: { err }});
-    } else {
-        if (err.status > 400 && err.status < 500) {
-            res.status(err.status).json({ status: "failed", data: { message: "Bad request" }});
-        } else {
-            res.status(err.status).json({ status: "failed", data: { message: "Internal Server Error" }});
-        }
-    }
-});
+const app = require("./app");
+const { PORT } = require("./config/Environments");
 
 app.listen(PORT, () => console.log(`Application listening on port: ${PORT}`));
